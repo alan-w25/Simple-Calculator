3d9eb99d1f3246f2ab28d423cbec7a8f290b292f
Alan <43737208+alan-w25@users.noreply.github.com>
2021-01-22 22:14:39 -0500

    complete

--- calc.js
+++ calc.js
@@ -12,7 +12,7 @@ class Calculator{
         this.operation = undefined
     }
     delete() {
-
+        this.currentOp = this.currentOp.toString().slice(0, -1)
     }
     appendNumber(number) { 
         if(number == '.' && this.currentOp.includes('.')){
@@ -21,7 +21,7 @@ class Calculator{
         this.currentOp = this.currentOp.toString() + number.toString()
         
     }
-   chooseOperation(operation) {
+    chooseOperation(operation) {
        if(this.currentOp == ''){
            return
        }
@@ -33,12 +33,63 @@ class Calculator{
         this.currentOp = ''
    }
    compute() {
+        let computation
+        const previous = parseFloat(this.previousOp)
+        const current = parseFloat(this.currentOp)
 
-   }
+        if(isNaN(previous) || isNaN(current) ){
+            return
+        }
+        switch (this.operation){
+            case '+':
+                computation = previous + current
+                break
+            case '-':
+                computation = previous-current
+                break
+            case '*':
+                computation = previous * current
+                break
+            case '÷':
+                computation = previous / current
+                break
+            case '%': 
+                computation = previous % current
+                break
+            default: 
+                return
+        }
+        this.currentOp = computation 
+        this.operation = undefined 
+        this.previousOp = '' 
+    }
+    getDisplayNumber(number){
+        const stringNumber = number.toString() 
+        const intDigits = parseFloat(stringNumber.split('.')[0])
+        const decDigits = stringNumber.split('.')[1]
+        let intDisplay 
+        if(isNaN(intDigits)){
+                intDisplay = ''
+        } else{
+                intDisplay = intDigits.toLocaleString('en', {MaximumFractionDigits: 0})
+        }
+        if(decDigits!=null){
+                return `${intDisplay}.${decDigits}`
+        } else{
+                return intDisplay
+        }
+
+    }        
+   
    update() {
-        this.currentElementText.innerText = this.currentOp
-        this.previousElementText.innerText = this.previousOp
-   }
+        this.currentElementText.innerText = this.getDisplayNumber(this.currentOp)
+        if(this.operation != null){
+            this.previousElementText.innerText = 
+            `${this.getDisplayNumber(this.previousOp)} ${this.operation}`
+        } else {
+            this.previousElementText.innerText = ''
+        }
+    }
 }
 
 const numButton = document.querySelectorAll('[data-number]')
@@ -65,6 +116,15 @@ opButton.forEach(button => {
     })
 })
 
-equalsButton.addEventListener('click',()=>{
+eqButton.addEventListener('click', button =>{
     calc.compute()
+    calc.update()
+})
+acButton.addEventListener('click', button =>{
+    calc.clear()
+    calc.update()
+})
+delButton.addEventListener('click', button =>{
+    calc.delete()
+    calc.update()
 })
